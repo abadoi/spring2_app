506257b57aec2265460716bfc92ffb2377594f09
Andrei Badoi <andreibadoi11@gmail.com>
2021-03-19 16:47:07 +0200

    added 2nd provider for fallback

--- src/main/java/com/andrei/demo/service/CurrencyService.java
+++ src/main/java/com/andrei/demo/service/CurrencyService.java
@@ -4,9 +4,16 @@ import com.andrei.demo.models.ConversionRequest;
 import com.andrei.demo.models.ConversionResponse;
 import com.andrei.demo.models.CurrencyAPIResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
+import reactor.core.Exceptions;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 
 @Service
@@ -24,6 +31,15 @@ public class CurrencyService implements ICurrencyService {
                         .build())
                 .retrieve()
                 .bodyToMono(CurrencyAPIResponse.class)
+                .retryWhen(Retry.backoff(3, Duration.ofSeconds(3)))
+                .onErrorResume(t -> Exceptions.isRetryExhausted(t), t ->
+                        webClient.get()
+                                .uri("https://api.exchangerate-api.com/v4/latest/" + e.getTo())
+                                .accept(MediaType.TEXT_PLAIN)
+                                .retrieve()
+                                .onStatus(HttpStatus::isError, response -> response.bodyToMono(String.class) // error body as String or other class
+                                        .flatMap(error -> Mono.error(new ResponseStatusException(response.statusCode(), "No conversion providers are currently available."))))
+                                .bodyToMono(CurrencyAPIResponse.class))
                 .map(currencyAPIResponse -> {
                     ConversionResponse response = new ConversionResponse();
                     response.setFrom(e.getFrom());
